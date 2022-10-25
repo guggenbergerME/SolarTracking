1259b7b94b351d4f210b819b483f737ce6dccbc6
TGuggenberger_peaq <github@guggeberger.me>
2022-10-25 16:46:34 +0200

    werte angepaasst

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
 /////////////////////////////////////////////////////////////////////////// Pin output zuweisen
-#define M1_re 2  // D2
-#define M1_li 4  // D4
-#define M2_re 5  // D5
-#define M2_li 18  // D18
+#define M1_re 2   // D2  - grau weiss - Pin 7
+#define M1_li 4   // D4  - grün - Pin 6
+#define M2_re 5   // D5  - violett - Pin 5
+#define M2_li 18  // D18 - schwarz - Pin 4
 
 /////////////////////////////////////////////////////////////////////////// Interrup
 int sturmschutzschalterpin =  13;
@@ -19,19 +19,20 @@ const int adc_SW = 32; //ADC1_9 - Fotowiderstand
 int sensorSonne_NO, sensorSonne_NW, sensorSonne_SO, sensorSonne_SW;
 int horizontal_hoch, horizontal_runter, vertikal_rechts, vertikal_links; 
 int differenz_neigen, differenz_drehen, sonne_quersumme, neigen_fahrt;
-int traker_tolleranz_neigen = 30; // Getestet mit 300
-int traker_tolleranz_drehen = 60;
-int helligkeit_schwellwert = 700; // Wolkenschwellwert
-int helligkeit_nachtstellung = 1400; // Wolkenschwellwert
+int traker_tolleranz_neigen = 90; // Getestet mit 300
+int traker_tolleranz_drehen = 120;
+int helligkeit_schwellwert = 750; // Wolkenschwellwert
+int helligkeit_nachtstellung = 1600; // Wolkenschwellwert
 
 /////////////////////////////////////////////////////////////////////////// Windsensor Variablen
 int wind_zu_stark = 0;
-int sturmschutz_pause = 5000000;
+int sturmschutz_pause = 45000;
 int pin_anemometer = 23; // Impulsgeber des Anemometer
 unsigned long start_time = 0;
-unsigned long end_time = 0;
+unsigned long end_time = 0
+;
 int steps = 0;
-int steps_schwellwert = 8;
+int steps_schwellwert = 50;
 
 /////////////////////////////////////////////////////////////////////////// Schleifen verwalten
 unsigned long previousMillis_Sturmcheck = 0; // Windstärke prüfen
@@ -106,7 +107,7 @@ Serial.println(helligkeit_schwellwert);
 // Justierung stoppen Wolken
 if (sonne_quersumme < helligkeit_schwellwert) {
 Serial.println("Helligkeit - Ausrichten ");
-
+//horizontal_hoch   = ((sensorSonne_NO + sensorSonne_NW)/2)*(-1);
 horizontal_hoch   = ((sensorSonne_NO + sensorSonne_NW)/2)*(-1);
 horizontal_runter = ((sensorSonne_SO + sensorSonne_SW)/2)*(-1);
 vertikal_rechts   = ((sensorSonne_NO + sensorSonne_SO)/2)*(-1);
@@ -120,16 +121,15 @@ vertikal_links    = ((sensorSonne_NW + sensorSonne_SW)/2)*(-1);
 
     if (vertikal_rechts > vertikal_links && (vertikal_rechts-vertikal_links) > traker_tolleranz_drehen) {
       Serial.println("Motor drehen - LINKS");
-      m2(1);  
-      // delay(1000);
+      m2(1); 
     } else {
       m2(3);
     }
     
     if (vertikal_links > vertikal_rechts && (vertikal_links-vertikal_rechts) > traker_tolleranz_drehen) {
-      Serial.println("Motor drehen - RECHTS");
+      Serial.println("XXXX Motor drehen - RECHTS");
       m2(2); 
-      // delay(1000);
+    //delay(1000);
     } else {
       m2(3);
     }
@@ -190,8 +190,8 @@ void m1(int x) {
   if (x == 1) {
     // Panele senken
     //Serial.println("Panele senken");
-    digitalWrite(M1_re, HIGH);
-    digitalWrite(M1_li, LOW);
+    digitalWrite(M1_re, HIGH); // D2 - weiss - Pin7
+    digitalWrite(M1_li, LOW); // D4 - grün + Pin 6
   }
 
   if (x == 2) {
@@ -217,8 +217,8 @@ void m2(int x) {
   if (x == 1) {
     // Panel links drehen
     //erial.println("Panel links drehen");
-    digitalWrite(M2_re, HIGH);
-    digitalWrite(M2_li, LOW);
+    digitalWrite(M2_re, HIGH); // D5 - violett - Pin 5
+    digitalWrite(M2_li, LOW); // D18 - schwarz - Pin 4
   }
 
   if (x == 2) {
@@ -267,7 +267,7 @@ void sturmschutz() {
 
         Serial.println("Wind zu stark Panele sichern ");
         // Panel in Position fahren
-        m1(2);
+        //m1(2);
         // Lange pause um Prozessor zu unterbrechen.
         delay (sturmschutz_pause);
         
@@ -321,7 +321,6 @@ void sturmschutzschalter() {
 /////////////////////////////////////////////////////////////////////////// LOOP
 void loop() {
 
-
   //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Auf Sturm prüfen
   if (millis() - previousMillis_Sturmcheck > interval_Sturmcheck) {
       previousMillis_Sturmcheck = millis(); 
@@ -364,7 +363,4 @@ void loop() {
   
     }
 
-
-
-delay(800);
 }
